743908ded3b3356686b2ac0f8cc190263c5e7054
pronet-lab <saad.ziauddin@pronet-tech.net>
2025-06-20 13:18:03 +0500

    commit

--- connection.php
+++ connection.php
@@ -1,6 +1,18 @@
 <?php
+    // $host = 'localhost';
+    // $db = 'khanjee_canada';
+    // $user = 'root';
+    // $pass = '';
 
-$connect = mysqli_connect('localhost','root','','khanjee_canada') or die(mysql_error());
-// echo "Working";
+    $host = 'sql100.infinityfree.com';
+    $db = 'if0_39277637_khanjee_canada';
+    $user = 'if0_39277637';
+    $pass = 'Saad8058';
 
+    $connect = mysqli_connect($host, $user, $pass, $db);
+    // if (!$connect) {
+    //     die("Connection failed: " . mysqli_connect_error());
+    // } else {
+    //     echo "Connection successful!";
+    // }
 ?>
